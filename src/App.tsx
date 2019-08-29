4a5199271ebd680a4484e4665aa5f774f1ae3967
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-08-29 22:39:25 +0700

    first commit with bulma

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <section className="section">
+      <div className="container">
+        <h1 className="title">
+          Hello World,
+        </h1>
+        <p className="subtitle">
+          <strong>Bulma</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <button className="button is-primary">Button</button>
+      </div>
+    </section>
   );
 }
 
