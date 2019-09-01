c5fcb529098a100b20e2d331ea7284bd78dc7b06
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-09-01 20:19:26 +0700

    adding interface state isLoggedIn

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-class Navbar extends React.Component {
+interface IState {
+    isLoggedIn : boolean
+}
+
+class Navbar extends React.Component<{}, IState> {
     constructor(props : never) {
         super(props)
+        this.state = {
+            isLoggedIn : false,
+        }
     }
     
     public render() {
+        const isLoggedIn = this.state.isLoggedIn
         return(
             <nav className="navbar is-primary is-fixed-top box-shadow-y">
                 <div className="navbar-brand">
@@ -24,16 +32,31 @@ class Navbar extends React.Component {
                 <div className="navbar-menu">
                     <div className="navbar-start"></div>
                     <div className="navbar-end">
-                        <a href="#" className="navbar-item">Notifications</a>
-                        <div className="navbar-item has-dropdown is-hoverable">
-                            <a href="#" className="navbar-link">Admin</a>
-                            <div className="navbar-dropdown is-right">
-                                <a href="#" className="navbar-item">Profile</a>
-                                <a href="#" className="navbar-item">Settings</a>
-                                <hr className="navbar-divider"/>
-                                <a href="#" className="navbar-item">Log out</a>
-                            </div>
-                        </div>
+                        {
+                            isLoggedIn ?
+                            (
+                                <React.Fragment>
+                                    <a href="#" className="navbar-item">Notifications</a>
+                                    <div className="navbar-item has-dropdown is-hoverable">
+                                        <a href="#" className="navbar-link">Admin</a>
+                                        <div className="navbar-dropdown is-right">
+                                            <a href="#" className="navbar-item">Profile</a>
+                                            <a href="#" className="navbar-item">Settings</a>
+                                            <hr className="navbar-divider"/>
+                                            <a href="#" className="navbar-item">Log out</a>
+                                        </div>
+                                    </div>
+                                </React.Fragment>
+                            ) :
+                            (
+                                <div className="navbar-item">
+                                    <div className="buttons">
+                                    <a href="#" className="button is-primary"> <strong> Sign Up </strong> </a>
+                                    <a href="#" className="button is-link"><strong> Sign In </strong></a>
+                                    </div>
+                                </div>
+                            )
+                        }
                     </div>
                 </div>
             </nav>
