86d89a7181935945c3b2cb43af3ded5f51cc18b8
Risky Feryansyah Pribadi <riskypribadi24@gmail.com>
2019-09-02 22:14:16 +0700

    adding navbar and box into index

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+
+import Navbar from './components/navbar/navbar';
+import Box from './components/box/box'
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.Fragment>
+        <Navbar />
+        <Box />
+    </React.Fragment>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
